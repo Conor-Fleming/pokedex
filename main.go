0e51d2ef930bab78602cd2f5c3d1f2bdeeb90471
Conor-Fleming <conorfleming03@gmail.com>
2023-02-28 17:10:13 -0800

    setting up banner, basic repl workflow with help and exit commands

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	BANNER = `
+====================================
+   _________________________                
+  / __  / __  /| |/ /  /___/
+ / /_/_/ /_/ / |   \  / __/ 
+/_/   /_____/  |_|\_\/___/
+                           
+====================================
+`
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Print(BANNER)
+	help()
+
+	commands := map[string]interface{}{
+		"help": help,
+		"exit": exit,
+	}
+
+	for {
+		fmt.Print("pokedex > ")
+		var input string
+		fmt.Scanf("%v", &input)
+		if value, ok := commands[input]; ok {
+			value.(func())()
+			continue
+		}
+		invalidCommand()
+	}
+}
+
+func invalidCommand() {
+	fmt.Println("Command not found")
+}
+
+func help() {
+	fmt.Println("\n Welcome to Pokedex! ")
+	fmt.Println("                             ")
+	fmt.Println("These Are the Avaliable commands: ")
+	fmt.Println()
+	fmt.Println("help   - Show you the Help")
+	fmt.Println("exit   - Exits the Go REPL ")
+}
+
+func exit() {
+	os.Exit(0)
+
 }
