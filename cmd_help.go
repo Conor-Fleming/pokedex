6f9aae49ad8185b197504f1664037c1b1743a95e
Conor-Fleming <conorfleming03@gmail.com>
2023-03-03 19:44:04 -0800

    cleaning up, gitignore, new banner

--- cmd_help.go
+++ cmd_help.go
@@ -9,6 +9,9 @@ func help(cfg *config) error {
 	fmt.Println()
 	fmt.Println("help   - Show you the Help")
 	fmt.Println("exit   - Exits the Go REPL ")
+	fmt.Println("map    - view the next page of locations ")
+	fmt.Println("mapb   - view the last page of locations ")
+	fmt.Println("-----------------------------------------")
 
 	return nil
 }
