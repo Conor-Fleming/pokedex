0158bbb8ce46b665d71043a6f4bbe22d2e5a37af
Conor-Fleming <conorfleming03@gmail.com>
2023-03-06 19:50:06 -0800

    starting catch pokemon probability func

--- cmd_catch.go
+++ cmd_catch.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/Conor-Fleming/pokedex/internal/pokeapi"
 )
@@ -14,15 +16,37 @@ func catch(cfg *pokeapi.Config, name ...string) error {
 	pokemonName := name[0]
 	//call catch pokemon func that returns a Pokemon obj and an error
 	//if catch returns error then print message and return
+	fmt.Printf("Throwing a Pokeball at %s\n", pokemonName)
 	pokemon, err := cfg.GetPokemon(pokemonName)
 	if err != nil {
 		return err
 	}
-	//do catch logic with using pokemon base experience
 
-	//if the catch func was successful add pokemon to pokedex and print success
-	fmt.Printf("%s was caught!", pokemonName)
+	//if the pokemon escaped, print message and return
+	if !catchPokemon(pokemon.BaseExperience) {
+		fmt.Printf("%s escaped!\n", pokemonName)
+		return nil
+	}
+
+	//if pokemon was caught print message and add to pokedex
+	fmt.Printf("%s was caught!\n", pokemonName)
 	cfg.Pokedex[pokemonName] = pokemon
 
 	return nil
+}
+
+func catchPokemon(exp int) bool {
+	// Calculate catch probability based on base experience
+	catchProbability := 1.0 / float64(exp)
+
+	// Generate random number between 0 and 1
+	rand.Seed(time.Now().UnixNano())
+	randomNum := rand.Float64()
+
+	// Compare random number to catch probability
+	if randomNum <= catchProbability {
+		return true // Pokémon caught!
+	} else {
+		return false // Pokémon escaped :(
+	}
 }
